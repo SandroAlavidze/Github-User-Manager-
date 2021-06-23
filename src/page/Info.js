464a127b0eac33b095f3914e5ab7b7cb12a549a4
Sandro Alavidze <sandro.alavidze@yahoo.com>
2021-06-23 23:44:38 +0400

    Info with loading

--- src/page/Info.js
+++ src/page/Info.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import Loader from "react-loader-spinner";
+import { Box } from "@material-ui/core";
+
 import { getInfo } from "../api/detinfo";
 import Fullinfo from "../components/Fullinfo";
 import Header from "../components/Header";
@@ -7,20 +10,30 @@ import Header from "../components/Header";
 function Info() {
   const login = useParams();
   const [fullinfo, setFullinfo] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    getInfo(login.id).then((data) => setFullinfo(data));
+    setLoading(true);
+    getInfo(login.id)
+      .then((data) => setFullinfo(data))
+      .finally(() => setLoading(false));
   }, []);
   return (
     <>
       <Header />
-      <Fullinfo
-        username={login.id}
-        avatar={fullinfo.avatar_url}
-        followers={fullinfo.followers}
-        following={fullinfo.following}
-        bio={fullinfo.bio}
-      />
+      {loading ? (
+        <Box display="flex" justifyContent="center">
+          <Loader type="Oval" color="#00BFFF" />
+        </Box>
+      ) : (
+        <Fullinfo
+          username={login.id}
+          avatar={fullinfo.avatar_url}
+          followers={fullinfo.followers}
+          following={fullinfo.following}
+          bio={fullinfo.bio}
+        />
+      )}
     </>
   );
 }
